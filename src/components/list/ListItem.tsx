4fc4d4a257d5a6cdda22eda5e05fe2050209b88e
Daniel Emery <daniel@sine.co>
2020-10-31 12:07:39 +1030

    Fixed list item compile error

--- src/components/list/ListItem.tsx
+++ src/components/list/ListItem.tsx
@@ -5,7 +5,11 @@ export interface IListItemProps {
   children: React.ReactNode;
 }
 
+const StyledListItem = styled.div`
+  padding: 1rem;
+`;
+
 export default function ListItem(props: IListItemProps) {
   const { children } = props;
-  return <div>{children}</div>;
+  return <StyledListItem>{children}</StyledListItem>;
 }
